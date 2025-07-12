fb45a02aa5c953d677693bb098b6fe0b3bd57c6d
Hao Ye <haoye.au@gmail.com>
2025-07-12 21:28:07 +1000

    Request autorization code

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,48 @@
-import { Text, View } from "react-native";
+import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
+import * as WebBrowser from "expo-web-browser";
+import { useEffect } from "react";
+import { Button } from "react-native";
 
-export default function Index() {
+WebBrowser.maybeCompleteAuthSession();
+
+const CLIENT_ID = "Ov23liVbQmkBAiSIuewZ";
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint: 'https://github.com/settings/connections/applications/' + CLIENT_ID,
+};
+
+export default function App() {
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: CLIENT_ID,
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'commitcadence',
+        path: 'github/callback',
+      }),
+      
+    },
+    discovery
+  );
+  
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params;
+      // Handle the authorization code here, e.g., exchange it for an access token
+      console.log('Authorization code:', code);
+      // TODO exchange code for access token
+    }
+  }, [response]);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+    <Button
+      title="Login with GitHub"
+      disabled={!request}
+      onPress={() => {
+        promptAsync();
       }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    />
   );
 }
